59604c78ac6ab9e95075331f0108d497bb9ad49d
linesh-simplicity <linesh.simpcity@gmail.com>
2016-04-08 21:18:40 +0800

    Linesh: Use angular 2's hash two way bind feature

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,17 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>yeah</h1>'
+    template: `<h1>Welcome to Angular 2 World</h1>
+        <div>
+            <input type="text" #angularInput>
+            <button (click)="onClick(angularInput.value)">Click Me</button>
+        </div>
+    `
+
 })
 
-export class AppComponent {}
+export class AppComponent {
+    onClick(value) {
+        console.log(value + 'clicked');
+    }
+}
